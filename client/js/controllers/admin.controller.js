253b3ecba9935e2ca61943df8b741b9fdbf36f44
Livne Lang <livnelang@gmail.com>
2016-04-01 16:27:06 +0300

    1.4.16 commit

--- client/js/controllers/admin.controller.js
+++ client/js/controllers/admin.controller.js
@@ -1,3 +1,13 @@
 app.controller('adminCtrl', function ($scope, $http, $window) {
- console.log('hi admin main control panel ..')
+ console.log('hi admin main control panel ..');
+    $scope.teacher_id = {"teacher_id": "997675"};
+
+ /**
+  * Returns Teacher Courses List
+  */
+ $http.post("http://localhost:3000/teachersCourses",$scope.teacher_id  ).success(function (data) {
+        console.dir('data: ' +  data.courses)
+    });
+
+
 });
